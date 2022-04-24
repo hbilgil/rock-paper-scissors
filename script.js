50bcd53c5a924f9d06cb37a0ff7af5f996b87aef
Halil Bilgil <hbilgil@gmail.com>
2022-04-24 11:29:37 +0300

    Edit single round function

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function computerPlay() {
 //User's choice will be compared by computer's selection
 
 function playRound(playerSelection, computerSelection) { //2 different parameters will be used for the comparison inside the function
+}
+
+//Parameters should be defined and
+//A pop-up box will be provided for the user enter his/her choice
+
+let text = (prompt("Please enter your choice: e.g. rock, paper or scissors")); //user enters choice in a pop-up box
+let playerSelection = text.toLowerCase(); // case-insensitive player choice converts a string to lowercase letters to be compared by computer's choice successfully
+let computerSelection = computerPlay();  //declaring a randomly selected choice by computer
