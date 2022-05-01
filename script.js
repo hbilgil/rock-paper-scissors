3f944126ec900f16b829869fdaae89c7214ed926
Halil Bilgil <hbilgil@gmail.com>
2022-05-01 21:33:15 +0300

    Add a new function changing the game-end dynamic

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+//Score variables should be defined
+
+let humanScore = 0;
+let computerScore = 0;
+let roundWinner ='';
+
+/*--------------new function---------------------
+Define a function for a single round in the game
+User's choice will be compared by computer's selection
+------------------------------------------------*/
+
+function playRound(playerSelection, computerSelection) { //2 different parameters will be used for the comparison inside the function
+  
+  //--new conditional for comparison--
+  
+  //User will win if below mentioned conditions occur
+    if (playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper"){
+      humanScore++; //humanScore variable will be defined in order to both increase and compare User's score
+      roundWinner = 'player' //win variable will be defined in order to show the result of the round if user wins
+    }
+  //User will lose if below mentioned conditions occur
+    else if (playerSelection=="rock" && computerSelection=="paper" || playerSelection=="scissors" && computerSelection=="rock" || playerSelection=="paper" && computerSelection=="scissors"){
+      computerScore++;//computerScore variable will be defined in order to both increase and compare Computer's score
+      roundWinner = 'computer'//lose variable will be defined in order to show the result of the round if user loses
+    }
+  //Neither side will win or lose if below mentioned condition occur
+      else (playerSelection == computerSelection){
+      //no need to define a tieScore in a 5 round game
+      roundWinner = 'tie'//tie variable will be defined in order to show the result of the round if neither side wins or loses
+    }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection);
+  }
+
 /*--------------new function---------------------
 Define a function allowing the computer make a choice among rock, paper and scissors
 Computer choice is triggered by function defined
@@ -10,34 +43,39 @@ function computerPlay() {
   return result;
 }
 
+let computerSelection = computerPlay();  //declaring a randomly selected choice by computer
+
+// UI DOM manipulations and event listeners
+
+const roundScoreInfo = document.getElementById('roundScoreInfo');
+const scoreMessage = document.getElementById('roundScoreMessage');
+const playerScorePara = document.getElementById('playerScore');
+const computerScorePara = document.getElementById('computerScore');
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+const rock = document.getElementById('rockButton');
+const paper = document.getElementById('paperButton');
+const scissors = document.getElementById('scissorsButton');
+
+const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const overlay = document.getElementById('overlay');
+const restartBtn = document.getElementById('restartBtn');
+
+rockButton.addEventListener('click', () => handleClick('ROCK'))
+paperButton.addEventListener('click', () => handleClick('PAPER'))
+scissorsButton.addEventListener('click', () => handleClick('SCISSORS'))
+restartBtn.addEventListener('click', restartGame)
+overlay.addEventListener('click', closeEndgameModal)
+
+
 /*--------------new function---------------------
-Define a function for a single round in the game
-User's choice will be compared by computer's selection
+Define a function allowing the game ends whichever 
+side has reached at 5 points
 ------------------------------------------------*/
 
-function playRound(playerSelection, computerSelection) { //2 different parameters will be used for the comparison inside the function
-  
-//--new conditional for comparison--
-
-//User will win if below mentioned conditions occur
-  if (playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper"){
-    humanScore++; //humanScore variable will be defined in order to both increase and compare User's score
-    return win; //win variable will be defined in order to show the result of the round if user wins
-  }
-//User will lose if below mentioned conditions occur
-  else if (playerSelection=="rock" && computerSelection=="paper" || playerSelection=="scissors" && computerSelection=="rock" || playerSelection=="paper" && computerSelection=="scissors"){
-    computerScore++;//computerScore variable will be defined in order to both increase and compare Computer's score
-    return lose;//lose variable will be defined in order to show the result of the round if user loses
-  }
-//Neither side will win or lose if below mentioned condition occur
-    else if (playerSelection == computerSelection){
-    //no need to define a tieScore in a 5 round game
-    return tie;//tie variable will be defined in order to show the result of the round if neither side wins or loses
-  }
-//User should be warned if he/she does not enter a value or makes a choice other than rock, paper or scissors
-  else {
-    return "You must write down rock, paper or scissors! Please try again.\n-----Round End-----";
-  }
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5
 }
 
 /*--------------new function---------------------
@@ -94,7 +132,7 @@ Define a function for updating score messages according to choices
 Score messages will be updated on the screen in each round
 ------------------------------------------------*/
 
-function updateScoreMessage(winner, playerSelection, computerSelection) {
+function updateScoreMessage(roundWinner, playerSelection, computerSelection) {
   if (roundWinner === 'player') {
     roundScoreMessage.textContent = `${capitalizeFirstLetter(
       playerSelection
@@ -113,38 +151,19 @@ function updateScoreMessage(winner, playerSelection, computerSelection) {
   )} ties with ${computerSelection.toLowerCase()}`
 }
 
+/*--------------new function---------------------
+Define a function for making a string's first letter capitalized
+------------------------------------------------*/
 
-//Function playRound() Parameters should be defined
-//A pop-up box will be provided for the user enter his/her choice
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
 
-let text = (prompt("Please enter your choice: e.g. rock, paper or scissors")); //user enters choice in a pop-up box
-let playerSelection = text.toLowerCase(); //case-insensitive player choice converts a string to lowercase letters to be compared by computer's choice successfully
-console.log (`You choose ${playerSelection}`); //show user's choice in lowercase
-
-let computerSelection = computerPlay();  //declaring a randomly selected choice by computer
-console.log(`The computer chooses ${computerSelection}`);//show computer's choice
-
-//Score variables should be defined
-
-let humanScore = 0;
-let computerScore = 0;
-let roundWinner ='';
-
-//Round-game result variables should be defined
-
-const win="You Won this round!\n-----Round End-----";
-const lose="You Lost this round!\n-----Round End-----";
-const tie= "This round is a tie!\n-----Round End-----";
-
-//Showing the Round-game results in the console
-
-console.log(playRound(playerSelection, computerSelection)); //Single round result will be shown
-
-console.log("Your score: " + humanScore + " " + "Computer score: " + computerScore);//The score will be counted according to the single round result
-
-//--------------new function---------------------
-//Define a function for looping a 5 round game
-//Final result and score will be defined too
+/*--------------new function---------------------
+Define a function for looping a game until  
+either player or computer reaches at 5 points
+Final result and score will be defined too
+------------------------------------------------*/
 
 function game() {
 
