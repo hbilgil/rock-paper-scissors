cf468730984da08441c9f3b3faf958502e38b71f
Halil Bilgil <hbilgil@gmail.com>
2022-05-01 23:35:36 +0300

    Edit script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //Score variables should be defined
 
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 let roundWinner ='';
 
@@ -16,21 +16,21 @@ function playRound(playerSelection, computerSelection) { //2 different parameter
   
   //User will win if below mentioned conditions occur
     if (playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper"){
-      humanScore++; //humanScore variable will be defined in order to both increase and compare User's score
-      roundWinner = 'player' //win variable will be defined in order to show the result of the round if user wins
+      playerScore++; //humanScore variable will be defined in order to both increase and compare User's score
+      roundWinner = 'player'; //win variable will be defined in order to show the result of the round if user wins
     }
   //User will lose if below mentioned conditions occur
-    else if (playerSelection=="rock" && computerSelection=="paper" || playerSelection=="scissors" && computerSelection=="rock" || playerSelection=="paper" && computerSelection=="scissors"){
+      else if (playerSelection=="rock" && computerSelection=="paper" || playerSelection=="scissors" && computerSelection=="rock" || playerSelection=="paper" && computerSelection=="scissors"){
       computerScore++;//computerScore variable will be defined in order to both increase and compare Computer's score
-      roundWinner = 'computer'//lose variable will be defined in order to show the result of the round if user loses
-    }
+      roundWinner = 'computer';//lose variable will be defined in order to show the result of the round if user loses
+      }
   //Neither side will win or lose if below mentioned condition occur
-      else (playerSelection == computerSelection){
+      else if (playerSelection == computerSelection) {
       //no need to define a tieScore in a 5 round game
-      roundWinner = 'tie'//tie variable will be defined in order to show the result of the round if neither side wins or loses
-    }
+      roundWinner = 'tie';//tie variable will be defined in order to show the result of the round if neither side wins or loses
+      }
   updateScoreMessage(roundWinner, playerSelection, computerSelection);
-  }
+}
 
 /*--------------new function---------------------
 Define a function allowing the computer make a choice among rock, paper and scissors
@@ -53,19 +53,19 @@ const playerScorePara = document.getElementById('playerScore');
 const computerScorePara = document.getElementById('computerScore');
 const playerChoice = document.getElementById('playerChoice');
 const computerChoice = document.getElementById('computerChoice');
-const rock = document.getElementById('rockButton');
-const paper = document.getElementById('paperButton');
-const scissors = document.getElementById('scissorsButton');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 const endgameMessage = document.getElementById('endgameMessage');
 const endgameMsg = document.getElementById('endgameMsg');
-const finalWindow = document.getElementById('finalWindow');
 const restartBtn = document.getElementById('restartBtn');
+const finalWindow = document.getElementById('finalWindow');
 
-rockButton.addEventListener('click', () => pushClick('ROCK'))
-paperButton.addEventListener('click', () => pushClick('PAPER'))
-scissorsButton.addEventListener('click', () => pushClick('SCISSORS'))
-restartBtn.addEventListener('click', restartGame)
-finalWindow.addEventListener('click', closeEndgameMessage)
+rock.addEventListener('click', () => pushClick('rock'));
+paper.addEventListener('click', () => pushClick('paper'));
+scissors.addEventListener('click', () => pushClick('scissors'));
+restartBtn.addEventListener('click', restartGame);
+finalWindow.addEventListener('click', closeEndgameMessage);
 
 
 /*--------------new function---------------------
@@ -74,7 +74,7 @@ side has reached at 5 points
 ------------------------------------------------*/
 
 function isGameOver() {
-  return playerScore === 5 || computerScore === 5
+  return playerScore === 5 || computerScore === 5;
 }
 
 /*--------------new function---------------------
@@ -88,9 +88,9 @@ function pushClick(playerSelection) {
   }
 
   computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection)
-  updateChoices(playerSelection, computerSelection)
-  updateScore()
+  playRound(playerSelection, computerSelection);
+  updateChoices(playerSelection, computerSelection);
+  updateScore();
 
   if (isGameOver()) {
     openEndgameMessage();
@@ -104,8 +104,8 @@ Define a function for restarting the game
 function restartGame() {
   playerScore = 0;
   computerScore = 0;
-  roundScoreInfo.textContent = 'Choose your weapon';
-  roundScoreMessage.textContent = 'First to score 5 points wins the game';
+  roundScoreInfo.textContent = 'Round-Winner';
+  roundScoreMessage.textContent = 'Whoever reaches the 5 points first, is the winner!';
   playerScorePara.textContent = 'Player: 0';
   computerScorePara.textContent = 'Computer: 0';
   playerChoice.textContent = '❔';
@@ -124,14 +124,14 @@ function openEndgameMessage() {
 }
 
 function closeEndgameMessage() {
-  endgameMessage.classList.remove('active')
-  finalWindow.classList.remove('active')
+  endgameMessage.classList.remove('active');
+  finalWindow.classList.remove('active');
 }
 
 function setFinalMessage() {
   return playerScore > computerScore
-    ? (endgameMsg.textContent = 'Congratulations, You Won this game!')
-    : (endgameMsg.textContent = 'Unfortunately, You Lost this game!')
+    ? (endgameMsg.textContent = 'Congratulations!!! \n You Won this game!')
+    : (endgameMsg.textContent = 'Upss!!! \n You Lost this game!')
 }
 
 /*--------------new function---------------------
@@ -142,26 +142,26 @@ User's & Computer's choices will be shown on the screen in each round
 function updateChoices(playerSelection, computerSelection) {
   switch (playerSelection) {
     case 'rock':
-      playerChoice.textContent = '✊'
-      break
+      playerChoice.textContent = '✊';
+      break;
     case 'paper':
-      playerChoice.textContent = '✋'
-      break
+      playerChoice.textContent = '✋';
+      break;
     case 'scissors':
-      playerChoice.textContent = '✌'
-      break
+      playerChoice.textContent = '✌';
+      break;
   }
 
   switch (computerSelection) {
     case 'rock':
-      computerChoice.textContent = '✊'
-      break
+      computerChoice.textContent = '✊';
+      break;
     case 'paper':
-      computerChoice.textContent = '✋'
-      break
+      computerChoice.textContent = '✋';
+      break;
     case 'scissors':
-      computerChoice.textContent = '✌'
-      break
+      computerChoice.textContent = '✌';
+      break;
   }
 }
 
@@ -172,15 +172,15 @@ User's & Computer's choices will be updated on the screen in each round
 
 function updateScore() {
   if (roundWinner === 'player') {
-    roundScoreInfo.textContent = 'You won this round!'
+    roundScoreInfo.textContent = 'You won this round!';
   } else if (roundWinner === 'computer') {
-    roundScoreInfo.textContent = 'You lost this round!'
+    roundScoreInfo.textContent = 'You lost this round!';
   } else {
-    roundScoreInfo.textContent = 'This round is a tie!'
+    roundScoreInfo.textContent = 'This round is a tie!';
   }
 
-  playerScorePara.textContent = `Player: ${playerScore}`
-  computerScorePara.textContent = `Computer: ${computerScore}`
+  playerScorePara.textContent = `Player: ${playerScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
 }
 
 /*--------------new function---------------------
@@ -192,25 +192,25 @@ function updateScoreMessage(roundWinner, playerSelection, computerSelection) {
   if (roundWinner === 'player') {
     roundScoreMessage.textContent = `${capitalizeFirstLetter(
       playerSelection
-    )} beats ${computerSelection.toLowerCase()}`
-    return
+    )} beats ${computerSelection.toLowerCase()}`;
+    return;
   }
     else if (roundWinner === 'computer') {
     roundScoreMessage.textContent = `${capitalizeFirstLetter(
       playerSelection
-    )} is beaten by ${computerSelection.toLowerCase()}`
-    return
+    )} is beaten by ${computerSelection.toLowerCase()}`;
+    return;
   }
     else {
     roundScoreMessage.textContent = `${capitalizeFirstLetter(
       playerSelection
-  )} ties with ${computerSelection.toLowerCase()}`
+  )} ties with ${computerSelection.toLowerCase()}`;
+  }
 }
-
 /*--------------new function---------------------
 Define a function for making a string's first letter capitalized
 ------------------------------------------------*/
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
